a6bc20426c7c38276acf38e11b40ca86553abf18
Nadiia Sharafutdinova <111244855+NadiiaSharafutdinova@users.noreply.github.com>
2022-12-18 11:40:47 +0100

    Added index.html, portfolio.html, image folder

---
+++ js/console.js
@@ -0,0 +1,15 @@
+Скрипт для просмотра данных отправки в "console": 
+
+    <script>
+        (() => {
+            document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+                e.preventDefault();
+
+                new FormData(e.currentTarget).forEach((value, name) =>
+                    console.log(`${name}: ${value}`),
+                );
+
+                e.currentTarget.reset();
+            });
+        })();
+    </script>
